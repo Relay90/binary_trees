f99b8c480fe2f71d5975610811e7e072b74d8081
Relay90 <motaungr511@gmail.com>
2024-02-29 13:13:20 -0800

    binary_trees

---
+++ 112-array_to_bst.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+
+/**
+ * array_to_bst - turns an array to a BST tree
+ * @array: array to turns to BST tree
+ * @size: size of array
+ * Return: BST tree from array
+ */
+bst_t *array_to_bst(int *array, size_t size)
+{
+	size_t i = 0;
+	bst_t *root = NULL;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	for (; i < size; i++)
+	{
+		bst_insert(&root, array[i]);
+	}
+
+	return (root);
+}
